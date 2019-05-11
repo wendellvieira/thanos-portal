95019ab4da1a89f77b515e577b4335494f8c634d
Wendell Vieira <wendell.vieiracunha@gmail.com>
2019-05-11 20:19:03 -0300

    Criar material e buffer para efeito de fumaça

---
+++ src/PaticleSetup/SmokeCreate.js
@@ -0,0 +1,20 @@
+export default function( texture, Setup) {
+    const SmokeGeo = new THREE.PlaneBufferGeometry(1000, 1000);
+    const SmokeMaterial = new THREE.MeshStandardMaterial({
+        map: texture,
+        transparent: true
+    });
+
+    for(let p = 0; p < 40; p++){
+        let Particle = new THREE.Mesh(SmokeGeo, SmokeMaterial);
+        Particle.position.set(
+           Math.random() * 1000-500,
+           Math.random() * 500-200,
+           25
+        );
+        Particle.rotation.z = Math.random() * 360;
+        Particle.material.opacity = 0.4;
+        Setup.SmokeParticles.push(Particle)
+        Setup.Scene.add(Particle);
+    }
+}
