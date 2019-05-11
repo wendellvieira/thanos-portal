56713bda9c5e8670b69ac39e5cf3a61f527a2233
Wendell Vieira <wendell.vieiracunha@gmail.com>
2019-05-11 20:18:30 -0300

    Criar material e buffer do portal

---
+++ src/PaticleSetup/PortalCreate.js
@@ -0,0 +1,19 @@
+export default function( texture, Setup ){
+    const PortalGeo = new THREE.PlaneBufferGeometry(350, 350);
+    const PortalMaterial = new THREE.MeshStandardMaterial({
+        map: texture,
+        transparent: true
+    });
+
+    for(let p = 880; p > 250; p--){
+        let Particle = new THREE.Mesh(PortalGeo, PortalMaterial);
+        Particle.position.set(
+            0.5 * p * Math.cos( (4 * p * Math.PI) / 180),
+            0.5 * p * Math.sin( (4 * p * Math.PI) / 180),
+            0.1 * p
+        );
+        Particle.rotation.z = Math.random() * 360;
+        Setup.PortalParticles.push(Particle);
+        Setup.Scene.add(Particle);
+    }
+}
